a4ebef86159a9a14212ce8018acb13519d57bca6
mshakir082 <mdshakirhussain298@gmail.com>
2022-03-29 21:42:41 +0530

    Home page

---
+++ data/data.js
@@ -0,0 +1,33 @@
+var homepageData=[
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h56/hd8/26535698268190/A21HPSKYBLUELIN_BLUE.jpg_136Wx204H",
+        name: "Van Heusen",
+        decs:"Checks Polyester Viscose Slim Mens Work Wear Suit",
+        price:"5999",
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h7f/h19/27145685303326/S22SUCSLFJ34184_RED.jpg_136Wx204H",
+        name: "Blackberrys",
+        decs:"Checks Polyester Viscose Terry Slim Fit Mens Suit",
+        price:"15245",
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/he3/h5b/17201448747038/A21YI0893DP_GREEN.jpg_136Wx204H",
+        name: "Yuris",
+        decs:"Lace Round Neck Cotton Womens Kurta Palazzo set",
+        price:"1225",
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/hcc/hb6/26536003010590/A21HPBLACK5BTNV_BLACK.jpg_136Wx204H",
+        name: "Van Heusen",
+        decs:"Checks Polyester Viscose Supper Slim fit Mens Work",
+        price:"10999",
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h4f/hf5/26804890304542/A21346DRHF7_TAN.jpg_136Wx204H",
+        name: "Yurisone",
+        decs:"Lace Round Neck Cotton Womens One set",
+        price:"2000",
+    },
+  ];
+  localStorage.setItem("homeData",JSON.stringify(homepageData));
