b43e8efaaa3b36d0bb03af58871cdfda5c155779
Vivek8210 <99641318+Vivek8210@users.noreply.github.com>
2022-03-29 21:06:23 +0530

    navbar onway

---
+++ navbar.js
@@ -0,0 +1,113 @@
+const navbar=()=>{
+    return `<div id="navbar_container">
+    <div id="left_icons">
+        <h4 ><a href=""><i class="fa-solid fa-store"></i>All Stores</a></h4>
+        <h4><a href=""><i class="fa-solid fa-address-card"></i>Contact Us</a></h4>
+        </div>
+    <div id="right_icons">
+        <h4><a href=""> <img src="https://prodstatic.shoppersstop.com/_ui/updated_path/images/newLogo.svg"></a></h4>
+  
+
+        <h4><input type="text" placeholder="Search Products & Brands"><a href=""><i class="fa-solid fa-magnifying-glass"></i></a></h4> 
+         <h4><a href=""><i class="fa-regular fa-heart"></i></a></h4>
+         <h4><a href=""><i class="fa-regular fa-cart-flatbed-suitcase"></i></a></h4>
+        <h4><a href=""><i class="fa-regular fa-user"></i></a></h4>
+   </div>
+</div>
+<div id="all_item_navbar">
+    <ul>
+    
+    <li><a href="">MEN</a>
+
+    <ul>
+    <li><a href="">Clothing</a></li>
+    <li><a href="">Footwear</a></li>
+    <li><a href="">Watches</a></li>
+    <li><a href="">Frarances</a></li>
+    <li><a href="">Jewelerry</a></li>
+    </ul>
+
+    <li><a href="">WOMEN</a>
+    
+        <ul>
+            <li><a href="">Clothing</a></li>
+            <li><a href="">Footwear</a></li>
+            <li><a href="">Watches</a></li>
+            <li><a href="">Frarances</a></li>
+            <li><a href="">Jewelerry</a></li>
+        </ul>
+    </li>
+    <li><a href="">KIDS</a>
+    
+        <ul>
+            <li><a href="">Boys</a></li>
+            <li><a href="">Girls</a></li>
+            <li><a href="">Footwear</a></li>
+            <li><a href="">Frarances</a></li>
+            <li><a href="">Watches</a></li>
+        </ul>
+    </li>
+    <li><a href="">WATCHES</a>
+    
+        <ul>
+            <li><a href="">Men Watches</a></li>
+            <li><a href="">Women Watches</a></li>
+            <li><a href="">Kids Watches</a></li>
+          
+        </ul>
+    </li>
+    <li><a href="">BEAUTY</a>
+        <ul>
+            <li><a href="">Skincare</a></li>
+            <li><a href="">Haircare</a></li>
+            
+          
+        </ul>
+    </li>
+    <li><a href="">HOMESTOP</a>
+        <ul>
+            <li><a href="">Kitchen & Dinning</a></li>
+            <li><a href="">Bath</a></li>
+            <li><a href="">Weeding</a></li>
+          
+        </ul>
+
+    </li>
+    <li><a href="">LUXE</a>
+        <ul>
+            <li><a href="">Haircare</a></li>
+            <li><a href="">Skincare</a></li>
+           
+          
+        </ul>
+    </li>
+    <li><a href="">GIFTS</a>
+        <ul>
+            <li><a href="">Gifts For Her</a></li>
+            <li><a href="">Gifts For Him</a></li>
+           
+          
+        </ul>
+    </li>
+    <li><a href="">BRANDS</a>
+        <ul>
+            <li><a href="">Luxury Brands</a></li>
+            <li><a href="">All Brands</a></li>
+           
+          
+        </ul>
+    </li>
+    <li><a href="">BARGAINS</a>
+        <ul>
+            <li><a href="">Women</a></li>
+            <li><a href="">Men</a></li>
+            <li><a href="">Kids</a></li>
+            <li><a href="">Beauty</a></li>
+            
+        </ul>
+    </li>
+</ul>
+</div>`
+}
+
+export default navbar
