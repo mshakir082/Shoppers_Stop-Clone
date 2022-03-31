ef02e1fa61e47ca8562dae1dc1f2f7a54726161e
Vivek8210 <99641318+Vivek8210@users.noreply.github.com>
2022-04-01 02:04:23 +0530

    all watch and cloth page

---
+++ scripts/kidsclothdata.js
@@ -0,0 +1,137 @@
+let kidsclothData=[
+    {
+       image_url:"https://sslimages.shoppersstop.com/sys-master/images/haf/h9a/26939582414878/A21CAVT775416YE_YELLOW.jpg_230Wx334H",
+        name:"Stop",
+        desc:"Poly Blend Round Neck Boys Brocade Jacket With Kurta Pyjama",
+        price:550,
+    },
+
+    {
+       image_url:"https://sslimages.shoppersstop.com/sys-master/images/ha9/h00/26992023601182/A21UCBBCMES1IYE_YELLOW.jpg_230Wx334H",
+        name:"United Colors Of Benetton",
+        desc:"",
+        price:555,
+    },
+
+    {
+       image_url:"https://sslimages.shoppersstop.com/sys-master/images/he4/h6f/26960198336542/S22711GDRESS29_PEACH.jpg_230Wx334H",
+        name:"Stop",
+        desc:"https://sslimages.shoppersstop.com/sys-master/images/he4/h6f/26960198336542/S22711GDRESS29_PEACH.jpg_230Wx334H",
+        price:675,
+    },
+
+    {
+       image_url:"https://sslimages.shoppersstop.com/sys-master/images/he5/h85/27102554030110/S22710BPTEE005_TEAL.jpg_230Wx334H",
+        name:"United Colors Of Benetton",
+        desc:"Solid Cotton Collar Neck Boys T-Shirt",
+        price:559,
+    },
+
+    {
+       image_url:"https://sslimages.shoppersstop.com/sys-master/images/hf4/h64/26939943387166/A21CAVT775417GR_GREEN.jpg_230Wx334H",
+        name:"Stop",
+        desc:"Solid Cotton Collar Neck Boys T-Shirt",
+        price:456,
+    },
+    {
+       image_url:"https://sslimages.shoppersstop.com/sys-master/images/h6a/hf7/17290144448542/A21INFBJNS46_GREY.jpg_230Wx334H",
+        name:"United Colors Of Benetton",
+        desc:"Solid Cotton Collar Neck Boys T-Shirt",
+        price:990,
+    },
+
+    {
+       image_url:"https://sslimages.shoppersstop.com/sys-master/images/h56/h28/16613495930910/S21GJB382C180KH_KHAKI.jpg_230Wx334H",
+        name:"Stop",
+        desc:"Solid Cotton Collar Neck Boys T-Shirt",
+        price:780,
+    },
+
+    {
+       image_url:"https://sslimages.shoppersstop.com/sys-master/images/h31/h19/26939569635358/A21CAVT775416WH_WHITE.jpg_230Wx334H",
+        name:"United Colors Of Benetton",
+        desc:"Solid Cotton Collar Neck Boys T-Shirt",
+        price:770,
+    },
+
+    {
+       image_url:"https://sslimages.shoppersstop.com/sys-master/images/h4b/h84/17355064999966/A21INFBJNS44_ICE.jpg_230Wx334H",
+        name:"Stop",
+        desc:"Solid Cotton Collar Neck Boys T-Shirt",
+        price:670,
+    },
+
+    {
+       image_url:"https://sslimages.shoppersstop.com/sys-master/images/he6/h76/16120077811742/S21710BTEE025_ROYAL_BLUE.jpg_230Wx334H",
+        name:"United Colors Of Benetton",
+        desc:"Solid Cotton Collar Neck Boys T-Shirt",
+        price:880,
+    },
+
+    {
+       image_url:"https://sslimages.shoppersstop.com/sys-master/images/h6f/ha6/17553443487774/A21INFBSRT66_BLACK.jpg_230Wx334H",
+        name:"United Colors Of Benetton",
+        desc:"Solid Cotton Collar Neck Boys T-Shirt",
+        price:670,
+    },
+
+    {
+       image_url:"https://sslimages.shoppersstop.com/sys-master/images/hf3/h9d/17466253017118/S21710BSHORT5_BLUE.jpg_230Wx334H",
+        name:"Stop",
+        desc:"Solid Cotton Collar Neck Boys T-Shirt",
+        price:650,
+    },
+
+    {
+       image_url:"https://sslimages.shoppersstop.com/sys-master/images/h30/h89/16639408275486/SS21740KP708_SAGE.jpg_230Wx334H",
+        name:"United Colors Of Benetton",
+        desc:"Solid Cotton Collar Neck Boys T-Shirt",
+        price:990,
+    },
+
+    {
+       image_url:"https://sslimages.shoppersstop.com/sys-master/images/hfa/h02/26999517773854/S22GJB739C672BL_BLUE.jpg_230Wx334H",
+        name:"Stop",
+        desc:"Solid Cotton Collar Neck Boys T-Shirt",
+        price:650,
+    },
+
+    {
+       image_url:"https://sslimages.shoppersstop.com/sys-master/images/h21/h04/27200712409118/S22ASBF727974TE_TEAL_GREY.jpg_230Wx334H",
+        name:"United Colors Of Benetton",
+        desc:"Solid Cotton Collar Neck Boys T-Shirt",
+        price:775,
+    },
+
+    {
+       image_url:"https://sslimages.shoppersstop.com/sys-master/images/hde/h47/16119826382878/S21710BTEE031_PEACH.jpg_230Wx334H",
+        name:"United Colors Of Benetton",
+        desc:"Solid Cotton Collar Neck Boys T-Shirt",
+        price:765,
+    },
+
+    {
+       image_url:"https://sslimages.shoppersstop.com/sys-master/images/he2/h2a/16325407997982/206094458_ASSORTED.jpg_230Wx334H",
+        name:"Stop",
+        desc:"Solid Cotton Collar Neck Boys T-Shirt",
+        price:654,
+    },
+
+    {
+       image_url:"https://sslimages.shoppersstop.com/sys-master/images/h84/hd0/27080732377118/S22ASBF741168LI_LIGHT_BLUE.jpg_230Wx334H",
+        name:"United Colors Of Benetton",
+        desc:"Solid Cotton Collar Neck Boys T-Shirt",
+        price:870,
+    },
+
+    {
+       image_url:"https://sslimages.shoppersstop.com/sys-master/images/hae/h5a/27102494654494/S22710BPTEE004_MANGO.jpg_230Wx334H",
+        name:"Stop",
+        desc:"",
+        price:875,
+    },
+
+
+]
+
+localStorage.setItem("kidsclothdata",JSON.stringify(kidsclothData))
