cfc282d0d8156f25a2fbe8fde67f671158b666aa
TabrezAlam98 <97460388+TabrezAlam98@users.noreply.github.com>
2022-03-31 14:44:30 +0530

    mian page complete without import export

--- data/data.js
+++ data/data.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 var homepageData=[
     {
         image_url:"https://sslimages.shoppersstop.com/sys-master/images/h56/hd8/26535698268190/A21HPSKYBLUELIN_BLUE.jpg_136Wx204H",
@@ -31,3 +32,269 @@ var homepageData=[
     },
   ];
   localStorage.setItem("homeData",JSON.stringify(homepageData));
+=======
+var mensClothingData=[
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/hc2/h40/26971782021150/S22SFCSLB930357_BLACK.jpg_230Wx334H",
+        name:"Louis Philippe Sports",
+        decs:"Solid Cotton Slim Fit Mens Casual, Shirts - Black ",
+        price:"1297",
+        strike_price:"2199",
+        offer:"41%off"
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h6e/h98/27005143941150/LFENT06_INDIGO.jpg_230Wx334H",
+        name:"Life",
+        decs:"Cotton Elastane Mid Rise Crop Fit Men Jeans -",
+        price:"1199",
+        strike_price:"",
+        offer:""
+
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/hd5/hb2/27005041082398/LF-ECOM12_TINT.jpg_230Wx334H",
+        name:"Life",
+        decs:"Cotton Elastane Mid Rise Crop Fit Men Jeans -",
+        price:"849",
+        strike_price:"1699",
+        offer:"50% off"
+
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h5c/h8a/26912193085470/IN-FU07_DARK_INDIGO.jpg_230Wx334H",
+        name:"Infuse",
+        decs:"Cotton Elastane Mid Rise Crop Fit Men Jeans -",
+        price:"999",
+        strike_price:"1999",
+        offer:"50% off"
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h44/h81/26970342326302/S22TFWSLBB27660_GREEN.jpg_230Wx334H",
+        name:"Van Heusen Sport",
+        decs:"Solid Cotton Blend Slim Fit Mens Trousers -",
+        price:"1299",
+        strike_price:"1999",
+        offer:"35% off"
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/ha6/h0b/27113439952926/SS22INFDNM08_TEAL.jpg_230Wx334H",
+        name:"Infuse",
+        decs:"Solid Cotton Regular Fit Mens T-Shirt - Teal",
+        price:"399",
+        strike_price:"799",
+        offer:"50% off"
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h4d/h67/17556129087518/AW21DQCN003_TEAL.jpg_230Wx334H",
+        name:"Infuse",
+        decs:" Printed Cotton Jersey Regular Fit Mens T-Shirt",
+        price:"349",
+        strike_price:"699",
+        offer:"50% off"
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/ha6/he4/16323933700126/205533116_ASSORTED.jpg_230Wx334H",
+        name:"Jockey",
+        decs:"Mens Solid Trunks - Pack of 2",
+        price:"538",
+        strike_price:"",
+        offer:""
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h8e/he0/26912037339166/IN-FU04_TINT.jpg_230Wx334H",
+        name:"Infuse",
+        decs:"Cotton Mid Rise Straight Crop Fit Mens Jeans",
+        price:"999",
+        strike_price:"1999",
+        offer:"50% off"
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h85/h8f/26840589565982/AW21IPT041_TEAL.jpg_230Wx334H",
+        name:"Infuse",
+        decs:"Printed Cotton Regular Fit Mens T-Shirt - Teal",
+        price:"399",
+        strike_price:"799",
+        offer:"50% off"
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h25/h02/26835199623198/S22STGRAFT104_BLACK.jpg_230Wx334H",
+        name:"Stop",
+        decs:"Printed Cotton Regular Fit Mens T-Shirt",
+        price:"599",
+        strike_price:"",
+        offer:""
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h49/h36/27005160947742/LF-ECOM11_INDIGO.jpg_230Wx334H",
+        name:"Life",
+        decs:"Cotton Elastane Mid Rise Slim Fit Mens Jeans",
+        price:"849",
+        strike_price:"1699",
+        offer:"50% off"
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h97/hca/26912196755486/IN-FU013_INDIGO.jpg_230Wx334H",
+        name:"Infuse",
+        decs:"Cotton Elastane Denim Knit Mens Shorts",
+        price:"799",
+        strike_price:"1599",
+        offer:"50% off"
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h92/h4b/15854566244382/204425065_GREEN.jpg_230Wx334H",
+        name:"Allen Solly",
+        decs:"SOLLY Mens Regular Fit Solid T-Shirt - Green",
+        price:"599",
+        strike_price:"1199",
+        offer:"50% off"
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h37/h04/27074115829790/S22SFWSLPF43152_BEIGE.jpg_230Wx334H",
+        name:"Van Heusen Sport",
+        decs:"Solid Cotton Slim Fit Mens Casual Wear Shirt",
+        price:"1499",
+        strike_price:"1999",
+        offer:"25% off"
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/hce/hc3/26959183839262/S22WMTS003066BL_BLACK.jpg_230Wx334H",
+        name:"Wrangler",
+        decs:" Printed Cotton Regular Fit Mens T-Shirt - Black",
+        price:"699",
+        strike_price:"999",
+        offer:"30% off"
+
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h41/hf8/27082054303774/S2250K505618YAF_WHITE.jpg_230Wx334H",
+        name:"Calvin Klein Jeans",
+        decs:"Solid Cotton Mens Casual Cap - White",
+        price:"2299",
+        strike_price:"",
+        offer:""
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h80/ha7/27005175889950/LF-ECOM13_INDIGO.jpg_230Wx334H",
+        name:"Life",
+        decs:"Cotton Elastane Mid Rise Slim Fit Mens Jeans - jeans",
+        price:"849",
+        strike_price:"1699",
+        offer:"50% off"
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/hec/hb5/27178253844510/LFENT01_BLACK.jpg_230Wx334H",
+        name:"Life",
+        decs:" Cotton Elastane Mid Slim Fit Mens Jeans - Black",
+        price:"1199",
+        strike_price:"",
+        offer:""
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/hb7/h94/26943614124062/S22212CR1HSCRBN_NAVY.jpg_230Wx334H",
+        name:"Lee Cooper",
+        decs:"Printed Cotton Regular Fit Mens T-Shirt - Navy",
+        price:"699",
+        strike_price:"",
+        offer:""
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/hb9/h00/26729945858078/S22STSTRPL104_DARK_MUSTARD.jpg_230Wx334H",
+        name:"Stop",
+        decs:"Stripes Cotton Blend Regular Fit Mens T-Shirt",
+        price:"999",
+        strike_price:"",
+        offer:""
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/hd7/haa/26440149663774/A21INFUSE26_BROWN.jpg_230Wx334H",
+        name:"Infuse",
+        decs:"Printed Rayon Comfort Fit Mens Casual Shirt",
+        price:"699",
+        strike_price:"1299",
+        offer:"50% off"
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/hcb/h60/26972151152670/S221575FSSTRGRE_GREEN.jpg_230Wx334H",
+        name:"Killer",
+        decs:"Solid Cotton Slim Fit Mens Casual Shirt",
+        price:"1595",
+        strike_price:"",
+        offer:""
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h8e/h05/27075815768094/S22SFCSSBF11466_NAVY.jpg_230Wx334H",
+        name:"Louis Philippe Sports",
+        decs:"Stripes Cotton Super Slim Fit Mens Casual Wear Shirt",
+        price:"2499",
+        strike_price:"",
+        offer:""
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h5c/h8f/27018482614302/IN-FU018_GREY.jpg_230Wx334H",
+        name:"Infuse",
+        decs:"Cotton Elastane Crop Fit Mens Jeans",
+        price:"999",
+        strike_price:"1999",
+        offer:"50% off"
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h7d/ha9/14983695237150/205011626_ASSORTED.jpg_230Wx334H",
+        name:"Jockey",
+        decs:"Mens Solid Briefs - Pack of 2",
+        price:"398",
+        strike_price:"",
+        offer:""
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/hcc/hcf/27081672392734/S22J320976CBKNA_NAVY.jpg_230Wx334H",
+        name:"Calvin Klein Jeans",
+        decs:"Stripes Cotton Stretch Slim Fit Mens Casual Shirt",
+        price:"5999",
+        strike_price:"",
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/B8AC9759D45547D9AEF177F0DE13B7C8/img/EC93832975174AA7B2C3E3A81F1EF140/205495488_9312_EC93832975174AA7B2C3E3A81F1EF140.jpg",
+        name:"Puma",
+        decs:" Mens Embroidered Cap",
+        price:"599",
+        strike_price:"",
+        offer:""
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h94/he2/17505785380894/A21JJ280259201_BLUE.jpg_230Wx334H",
+        name:"Jack And Jones",
+        decs:"Solid Cotton Slim Fit Mens T-Shirt",
+        price:"2799",
+        strike_price:"",
+        offer:""
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/he0/hc8/26961613946910/S22YENSRSSIPWHI_WHITE.jpg_230Wx334H",
+        name:"Pepe",
+        decs:"Printed Cotton Slim Fit Mens T-Shirt",
+        price:"1299",
+        strike_price:"",
+        offer:""
+
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h0c/haf/16512812285982/S21SEABELONGKHA_KHAKI.jpg_230Wx334H",
+        name:"Celio Jeans",
+        decs:"Solid Cotton Straight Fit Mens T-Shirt",
+        price:"599",
+        strike_price:"999",
+        offer:"40% off"
+    },
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h47/h0c/27109667078174/SS22LKFSJACQURD_PINK.jpg_230Wx334H",
+        name:"Bandeya",
+        decs:"Printed Poly Silk Slim Fit Mens Festive Wear Kurta",
+        price:"1999",
+        strike_price:"",
+        offer:""
+    },
+    ]
+    localStorage.setItem("mensCotheData",JSON.stringify(mensClothingData));
+>>>>>>> Stashed changes
+
+
---
+++ scripts/mensClothe.js
@@ -0,0 +1,115 @@
+
+// --------------slide images----------------------
+
+const slideImagfun=(images,slideshow)=>{
+
+    let img = document.createElement("img");
+    img.src = images[0];
+    let i = 0;
+    setInterval( ()=> {
+       if (i == images.length) {
+              i = 0;
+            }
+    let imgUrl = images[i++];
+    let img = document.createElement("img");
+    img.src = imgUrl;
+    img.setAttribute("style", "width:100%;  height: 200px;");
+    slideshow.innerHTML = null;
+    slideshow.append(img)
+    }, 2000);
+ }
+//  slideImagfun(images)
+
+
+const mensDatashow=(mensClothingData)=>{
+   
+  document.getElementById("main").innerHTML=null
+    mensClothingData.forEach(({image_url, name, decs,  price, strike_price, offer})=>{
+
+      
+      let rowDiv=document.createElement("div");
+         rowDiv.id="homeDataRowDiv";
+
+         
+
+         let img=document.createElement("img");
+         img.src=image_url;
+
+         let Title = document.createElement("h3");
+         Title.innerText=name;
+         
+         
+
+         let descriptions= document.createElement("p");
+         descriptions.innerText=decs;
+         descriptions.id="desc";
+
+
+         let priceTag = document.createElement("h5");
+         priceTag.innerText=`₹${price}`;
+         priceTag.id="priceTag";
+
+         let priceTag2 = document.createElement("h5");
+         if(strike_price == ""){
+         
+         priceTag2.innerText=`${strike_price}`;
+         priceTag2.id="priceTag2"
+       
+      }
+      else{
+         
+         priceTag2.innerText=`₹${strike_price}`;
+         priceTag2.id="priceTag2"
+         
+
+      }
+
+
+         let priceOffer = document.createElement("h5");
+         priceOffer.innerText=` ${offer}`;
+         priceOffer.id="offers";
+
+
+         let priceDiv=document.createElement("div");
+         priceDiv.id="priceDiv";
+
+
+         let but= document.createElement("button");
+         but.innerText=`Add To Bag`;
+         but.id="butAddToBag"
+
+         let hrTag= document.createElement("hr");
+         hrTag.id="hrTag"
+         
+         priceDiv.append(priceTag,priceTag2,priceOffer)
+         rowDiv.append(img,Title,descriptions ,priceDiv);
+     append(rowDiv);
+   })
+}
+export  {slideImagfun,mensDatashow} 
+
+
+
+   //  ------------Sort Function--------------
+
+
+   document.getElementById("sorter").addEventListener("click",() =>{
+      
+      var sorting=document.getElementById("sorter").value
+      console.log(sorting)
+      if(sorting=='h2l'){
+         mensClothingData.sort((a,b)=>{
+              return b.price - a.price;
+          })
+          mensDatashow(mensClothingData)
+      }
+      else if(sorting=='l2h'){
+         mensClothingData.sort(function (a,b){
+              return a.price - b.price;
+          })
+          mensDatashow(mensClothingData)
+      }
+   
+   });
+  
+
