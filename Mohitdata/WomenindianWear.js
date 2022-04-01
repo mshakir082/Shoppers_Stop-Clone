5042f2ffbe2ec0b87ed8c3d59c0e4cb00bfc7931
Mohit Agrawal <agrawalmohitam@gmail.com>
2022-04-01 22:38:40 +0530

    Data_Files

---
+++ Mohitdata/WomenindianWear.js
@@ -0,0 +1,256 @@
+
+   
+
+var indianwomenData= [
+
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h3e/h91/26759827095582/S22STEPP19_SKY_BLUE.jpg_1088Wx1632H",
+        image2:"https://sslimages.s3-ap-southeast-1.amazonaws.com/sys-master/images/h87/ha5/26759980679198/S22STEPP19_SKY_BLUE_alt2.jpg_1088Wx1632H",
+        name:"Stop",
+        title:"Printed Rayon Mandarin Womens Knee Length Kurta",
+        price:559,
+        strike_price:"999",
+        offer:"40% OFF",
+        
+    },
+
+
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/hec/h03/27019664818206/S22ZIY3443ANDPA_BLACK.jpg_1088Wx1632H",
+        image2:"https://sslimages.shoppersstop.com/sys-master/images/h23/h89/27017652666398/S22ZIY3443ANDPA_BLACK_alt2.jpg_1088Wx1632H",
+        name:"Ziyaa",
+        title:"Floral Round Neck Rayon Womens Ethnic Set",
+        price: 885,
+        strike_price:"",
+        offer:"",
+        
+    },
+
+
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/hae/h16/17435690401822/A21112510618_CORAL.jpg_1088Wx1632H",
+        image2:"https://sslimages.shoppersstop.com/sys-master/images/h7e/h38/17435874164766/A21112510618_CORAL_alt2.jpg_1088Wx1632H",
+        name:"Varanga",
+        title:"Embroidered Silk Round Neck Womens Calf Length Kurta",
+        price:899,
+        strike_price:"",
+        offer:"",
+        
+    },
+
+
+
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h87/h7d/27016544845854/S22ZIR3439ANDPL_BLACK.jpg_1088Wx1632H",
+        image2:"https://sslimages.shoppersstop.com/sys-master/images/h8b/h49/27019281432606/S22ZIR3439ANDPL_BLACK_alt2.jpg_1088Wx1632H",
+        name:"Ziyaa",
+        title:"Floral Round Neck Crepe Womens Ethnic Set",
+        price:792,
+        strike_price:"",
+        offer:"",
+        
+    },
+
+
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h02/hae/26760041234462/AW21STKU1015_SAGE.jpg_1088Wx1632H",
+        image2:"https://sslimages.shoppersstop.com/sys-master/images/h82/ha1/26760009187358/AW21STKU1015_SAGE_alt2.jpg_1088Wx1632H",
+        name:"Stop",
+        title:"Rayon Womens 3/4th Sleeves Kurta",
+        price:1199,
+        strike_price:"",
+        offer:"",
+
+        
+    },
+
+
+
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h93/h5f/27018760945694/S22ZIR3502ANDPA_PINK.jpg_1088Wx1632H",
+        image2:"https://sslimages.shoppersstop.com/sys-master/images/hfb/h7e/27019144069150/S22ZIR3502ANDPA_PINK_alt2.jpg_1088Wx1632H",
+        name:"Ziyaa",
+        title:"Floral Mandarin Crepe Womens Ethnic Set",
+        price:1196,
+        strike_price:"",
+        offer:"",
+        
+    },
+
+
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h54/hf6/26852837785630/A21INPT2110FB_RED.jpg_1088Wx1632H",
+        image2:"https://sslimages.shoppersstop.com/sys-master/images/hb9/h87/26852692361246/A21INPT2110FB_RED_alt2.jpg_1088Wx1632H",
+        name:"Infuse",
+        title:"Printed Rayon Crape Round Neck Womens Pin Tuck Top",
+        price: 349,
+        strike_price:"599",
+        offer:"40% OFF",
+        
+    },
+
+
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h3c/h52/27016122073118/S22ZIY3367ANDPA_PEACOCK.jpg_1088Wx1632H",
+        image2:"https://sslimages.shoppersstop.com/sys-master/images/hc7/h96/27019042619422/S22ZIY3367ANDPA_PEACOCK_alt2.jpg_1088Wx1632H",
+        name:"Ziyaa",
+        title:"Floral Mandarin Rayon Womens Ethnic Set",
+        price:974,
+        strike_price:"",
+        offer:"",
+        
+    },
+
+
+
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/he3/h30/27019171921950/S22ZIY3455ANDPA_PINK.jpg_1088Wx1632H",
+        image2:"https://sslimages.shoppersstop.com/sys-master/images/hfa/hf1/27016849063966/S22ZIY3455ANDPA_PINK_alt2.jpg_1088Wx1632H",
+        name:"Ziyaa",
+        title:"Printed Round Neck Rayon Womens Ethnic Set",
+        price:972,
+        strike_price:"",
+        offer:"",
+        
+    },
+
+
+
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h1e/h4a/26759963869214/S22STEPP17_INDIGO.jpg_1088Wx1632H",
+        image2:"https://sslimages.shoppersstop.com/sys-master/images/h53/hd3/26759847968798/S22STEPP17_INDIGO_alt2.jpg_1088Wx1632H",
+        name:"Stop",
+        title:"Printed Cotton Mandarin Womens Knee Length Kurta",
+        price: 559,
+        strike_price:"999",
+        offer:"40% OFF",
+        
+    },
+
+
+
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h75/h39/26852871536670/A21INPT2116F_WHITE.jpg_1088Wx1632H",
+        image2:"https://sslimages.shoppersstop.com/sys-master/images/h56/h43/26852848795678/A21INPT2116F_WHITE_alt2.jpg_1088Wx1632H",
+        name:"Infuse",
+        title:"Printed Rayon Round Neck Womens Pin Tuck Top",
+        price:349,
+        strike_price:"",
+        offer:"",
+        
+    },
+
+
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/hca/h0e/17545312043038/A21STEPP77_SKY_BLUE.jpg_1088Wx1632H",
+        image2:"https://sslimages.shoppersstop.com/sys-master/images/h1f/h14/17544964440094/A21STEPP77_SKY_BLUE_alt2.jpg_1088Wx1632H",
+        name:"Stop",
+        title:"Printed Cotton Mandarin Womens Knee Length Kurta",
+        price:399,
+        strike_price:"",
+        offer:"",
+        
+    },
+
+
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h4a/hb2/27141195497502/A21ITP00985_PINK.jpg_1088Wx1632H",
+        image2:"https://sslimages.shoppersstop.com/sys-master/images/hab/h58/27140434984990/A21ITP00985_PINK_alt2.jpg_1088Wx1632H",
+        name:"Indya",
+        title:"Shraddha Kapoor For Indya Blush Sequin Strap Crop Top with Attached Dupatta",
+        price:1800,
+        strike_price:"",
+        offer:"",
+        
+    },
+
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h9a/hf0/27016690761758/S22VRNR97_MAUVE.jpg_1088Wx1632H",
+        image2:"https://sslimages.shoppersstop.com/sys-master/images/h26/h5c/27017057599518/S22VRNR97_MAUVE_alt2.jpg_1088Wx1632H",
+        name:"Varanga",
+        title:"Printed Round Neck Modal Womens Straight Kurta",
+        price:749,
+        strike_price:"1499",
+        offer:"50% OFF",
+        
+    },
+
+
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h88/hc8/27067471429662/S22JU2977D_PINK.jpg_1088Wx1632H",
+        image2:"https://sslimages.shoppersstop.com/sys-master/images/hec/ha4/27067026800670/S22JU2977D_PINK_alt2.jpg_1088Wx1632H",
+        name:"Juniper",
+        title:"Printed Rayon Round Neck Womens Straight Kurta",
+        price: 849,
+        strike_price:"",
+        offer:"",
+        
+    },
+
+
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/hcd/hb4/26973466656798/S22FEA80002D_GREEN.jpg_1088Wx1632H",
+        image2:"https://sslimages.shoppersstop.com/sys-master/images/hca/h91/26975023857694/S22FEA80002D_GREEN_alt2.jpg_1088Wx1632H",
+        name:"Aurelia",
+        title:"Printed Man Made Fibre Womens Casual Dupatta",
+        price: 799,
+        strike_price:"",
+        offer:"",
+        
+        
+    },
+
+
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/hec/hea/26972690743326/A21W215604X_MAROON.jpg_1088Wx1632H",
+        image2:"https://sslimages.shoppersstop.com/sys-master/images/hef/h9c/26974316593182/A21W215604X_MAROON_alt2.jpg_1088Wx1632H",
+        name:"W",
+        title:"Solid Round Neck Viscose Womens Calf Length Kurta",
+        price: 899,
+        strike_price:"1499",
+        offer:"40% OFF",
+        
+    },
+
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/hdd/h0c/27017184215070/S22ZIS2887ANDPA_CREAM.jpg_1088Wx1632H",
+        image2:"https://sslimages.shoppersstop.com/sys-master/images/hdf/h40/27016305049630/S22ZIS2887ANDPA_CREAM_alt2.jpg_1088Wx1632H",
+        name:"Ziyaa",
+        title:"Geometric Round Neck Polyester Womens Ethnic Set",
+        price: 996,
+        strike_price:"",
+        offer:"",
+        
+    },
+
+
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/h0b/hff/17427557089310/A219701OA_FUCHSIA.jpg_1088Wx1632H",
+        image2:"https://sslimages.shoppersstop.com/sys-master/images/he7/hf1/17427646939166/A219701OA_FUCHSIA_alt2.jpg_1088Wx1632H",
+        name:"Juniper",
+        title:"Embellished Silk Blend Mandarin Womens Regular Fit Kurta",
+        price: 1349,
+        strike_price:"",
+        offer:"",
+        
+    },
+
+
+    {
+        image_url:"https://sslimages.shoppersstop.com/sys-master/images/he3/h0e/27018839883806/S22ZIR2831ANDPA_BLACK.jpg_1088Wx1632H",
+        image2:"https://sslimages.shoppersstop.com/sys-master/images/h6a/h5d/27017862414366/S22ZIR2831ANDPA_BLACK_alt2.jpg_1088Wx1632H",
+        name:"Ziyaa",
+        title:"Floral Round Neck Crepe Womens Ethnic Set",
+        price:863,
+        strike_price:"",
+        offer:"",
+        
+    },
+
+
+];
+
+
+
+
+localStorage.setItem("indianwomenData",JSON.stringify(indianwomenData)) ;
---
+++ Mohitdata/girlswear.js
@@ -0,0 +1,245 @@
+let girlsWear = [
+    {
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h52/h2d/26414137507870/AW21740GWN1016_PINK.jpg_1088Wx1632H",
+   name:"Stop",
+   decs:"Poly Blend Fabric Round Neck Regular Fit Girls Festive Wear Gown",
+   price:"1399",
+   strike_price:"Rs 1999",
+   offer:"30% OFF",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/he6/hce/27095956946974/S22710GDRESS11_WHITE.jpg_1088Wx1632H",
+   name:"Stop",
+   decs:"Schiffli Cotton Round Neck Girls Casual Dress",
+   price:"899",
+   strike_price:"",
+   offer:"",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/haf/h7a/26476892487710/AW21740GC1025_PINK.jpg_1088Wx1632H",
+   name:"Stop",
+   decs:"Poly Blend Fabric Round Neck Regular Fit Girls Ghagra Choli Set",
+   price:"1399",
+   strike_price:"Rs 1999",
+   offer:"30% OFF",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h09/h47/27004490448926/SS21740DRS713_YELLOW.jpg_1088Wx1632H",
+   name:"Stop",
+   decs:"Poly Blend Round Neck Girls Tiered Printed Gown",
+   price:"699",
+   strike_price:"",
+   offer:"",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/hfe/hac/26781066821662/S22710GDRS213_WHITE.jpg_1088Wx1632H",
+   name:"Stop",
+   decs:"Printed Cotton Round Neck Girls Casual Knit Dress",
+   price:"399",
+   strike_price:"",
+   offer:"",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/hed/hcb/26960197746718/S22711GDRESS29_PEACH.jpg_1088Wx1632H",
+   name:"Life",
+   decs:"Swiss Dobby Polyester Round Neck Girls Casual Dress",
+   price:"799",
+   strike_price:"",
+   offer:"",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h41/h61/27032862130206/S22PPM2214682MU_MULTI.jpg_1088Wx1632H",
+   name:"Peppermint",
+   decs:"Abstract Cotton Blend Collared Girls Casual Wear Dresses",
+   price:"1495",
+   strike_price:"",
+   offer:"",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/he8/hb7/27066149535774/S22710GTOP53_PEACH.jpg_1088Wx1632H",
+   name:"Stop",
+   decs:"Applique Cotton Round Neck Girls T-Shir",
+   price:"399",
+   strike_price:"",
+   offer:"",
+
+
+
+},
+
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h31/h13/26933335883806/S22711GDRS222_NAVY.jpg_1088Wx1632H",
+   name:"Life",
+   decs:"Printed Cotton Round Neck Girls Casual Knit Dress",
+   price:"499",
+   strike_price:"",
+   offer:"",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/hf9/h56/16313447284766/S21CUTCC4760DPI_PINK.jpg_1088Wx1632H",
+   name:"Cutecumber",
+   decs:"Embellished Round Neck Organza & Net Girls Regular Dress",
+   price:"1850",
+   strike_price:"",
+   offer:"",
+
+
+
+},
+
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h1a/h77/16162199240734/SS21740CROP702_ROYAL_BLUE.jpg_1088Wx1632H",
+   name:"Stop",
+   decs:"Polyester Blend Round Neck Girls Crop Top With Embroidery",
+   price:"249",
+   strike_price:"Rs 499",
+   offer:"50% OFF",
+
+
+
+},
+
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h4f/h87/26504278016030/AW21740GC1040_PINK.jpg_1088Wx1632H",
+   name:"Stop",
+   decs:"Poly Blend Fabric Round Neck Regular Fit Girls Ghagra Choli Set",
+   price:"1329",
+   strike_price:"Rs 1899",
+   offer:"30% OFF",
+
+
+
+},
+
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h2a/hb0/26781074882590/S22710GDRS209_NAVY.jpg_1088Wx1632H",
+   name:"Stop",
+   decs:"Printed Cotton Round Neck Girls Casual Knit Dress",
+   price:"399",
+   strike_price:"",
+   offer:"",
+
+
+
+},
+
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h89/hd4/16162615197726/SS21740CROP701_YELLOW.jpg_1088Wx1632H",
+   name:"Stop",
+   decs:"Polyester Blend Round Neck Girls Crop Top With Embroidery",
+   price:"249",
+   strike_price:"Rs 499",
+   offer:"50% OFF",
+
+
+
+},
+
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h3d/h20/26504261828638/A21710GPDRS915_FUSCHIA.jpg_1088Wx1632H",
+   name:"Stop",
+   decs:"Solid Poly Blend Crew Neck Girls Dress",
+   price:"1299",
+   strike_price:"",
+   offer:"",
+
+
+
+},
+
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/hc2/hc6/26478040711198/AW21740GC1026_MINT.jpg_1088Wx1632H",
+   name:"Stop",
+   decs:"Poly Blend Fabric Round Neck Regular Fit Girls Ghagra Choli Set",
+   price:"1399",
+   strike_price:"1999",
+   offer:"30% OFF",
+
+
+
+},
+
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/hdb/h91/15898908164126/206710842_WHITE.jpg_1088Wx1632H",
+   name:"Gini & Jony",
+   decs:"Girls Regular Fit Printed Round Neck Top",
+   price:"349",
+   strike_price:"Rs 499 ",
+   offer:"30% OFF",
+
+
+
+},
+
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h24/hb0/26934079127582/S22711GDSHRT04_MID_STONE.jpg_1088Wx1632H",
+   name:"Life",
+   decs:"Embroidered Cotton Elastane Regular Fit Girls Denim Shorts",
+   price:"799",
+   strike_price:"Rs 299",
+   offer:"",
+
+
+
+},
+
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h00/h4b/26781213491230/S22710GSHRT005_LILAC.jpg_1088Wx1632H",
+   name:"Stop",
+   decs:"Printed Cotton Regular Fit Girls Knit Shorts",
+   price:"399",
+   strike_price:"",
+   offer:"",
+
+
+
+},
+
+
+
+   ];
+
+
+   localStorage.setItem("girlsWear",JSON.stringify(girlsWear)) ;
