c5bab8a21da6299606a2d70405f9428b6bb21110
Mohit Agrawal <agrawalmohitam@gmail.com>
2022-04-02 18:05:27 +0530

    Resolving Merge Conflict

---
+++ Mohit/Mohitdata/boyswear.js
@@ -0,0 +1,250 @@
+
+
+let boysWear = [
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/haf/h9a/26939582414878/A21CAVT775416YE_YELLOW.jpg_230Wx334H",
+   name:"Cavio",
+   decs:"Textured Cotton Round Neck Boys T-Shirt",
+   price:"265",
+   strike_price:"295",
+   offer:"10% OFF",
+
+
+
+},
+
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h37/h09/26992023339038/A21UCBBCMES1IYE_YELLOW.jpg_1088Wx1632H",
+   name:"United Colors Of Benetton",
+   decs:"Solid Cotton Blend Round Neck Boys T-Shirt",
+   price:"499",
+   strike_price:"",
+   offer:"",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h36/h5e/26939943190558/A21CAVT775417GR_GREEN.jpg_1088Wx1632H",
+   name:"Cavio",
+   decs:"Textured Cotton Round Neck Boys T-Shirt",
+   price:"265",
+   strike_price:"295",
+   offer:"10% OFF",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h61/hd4/27102553833502/S22710BPTEE005_TEAL.jpg_1088Wx1632H",
+   name:"Stop",
+   decs:"Solid Cotton Collar Neck Boys T-Shirt",
+   price:"399",
+   strike_price:"",
+   offer:"",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h4b/hb0/17290143989790/A21INFBJNS46_GREY.jpg_1088Wx1632H",
+   name:"Infuse",
+   decs:"Solid Denim Regular Fit Boys Shorts",
+   price:"349",
+   strike_price:"699",
+   offer:"50% OFF",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/hff/h22/26939569373214/A21CAVT775416WH_WHITE.jpg_1088Wx1632H",
+   name:"Cavio",
+   decs:"Textured Cotton Round Neck Boys T-Shirt",
+   price:"265",
+   strike_price:"295",
+   offer:"10% OFF",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/hae/h90/16639408013342/SS21740KP708_SAGE.jpg_1088Wx1632H",
+   name:"Stop",
+   decs:"Poly Blend Round Neck Boys Brocade Jacket With Kurta Pyjama",
+   price:"749",
+   strike_price:"1499",
+   offer:"50% OFF",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h25/h2f/16613495734302/S21GJB382C180KH_KHAKI.jpg_1088Wx1632H",
+   name:"Gini & Jony",
+   decs:"Printed Cotton Regular Fit Boys Shorts",
+   price:"389",
+   strike_price:"649",
+   offer:"40% OFF",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/hde/h7f/17355064803358/A21INFBJNS44_ICE.jpg_1088Wx1632H",
+   name:"Infuse",
+   decs:"Solid Denim Regular Fit Boys Shorts",
+   price:"349",
+   strike_price:"699",
+   offer:"50% OFF",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/hde/h7f/17355064803358/A21INFBJNS44_ICE.jpg_1088Wx1632H",
+   name:"Infuse",
+   decs:"Solid Denim Regular Fit Boys Shorts",
+   price:"349",
+   strike_price:"699",
+   offer:"50% OFF",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/hb6/h7d/16120077615134/S21710BTEE025_ROYAL_BLUE.jpg_1088Wx1632H",
+   name:"Stop",
+   decs:"Boys Solid Embroidered T-Shirt",
+   price:"199",
+   strike_price:"399",
+   offer:"50% OFF",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h54/h42/17466252460062/S21710BSHORT5_BLUE.jpg_1088Wx1632H",
+   name:"Stop",
+   decs:"Printed Cotton Regular Boys Shorts",
+   price:"279",
+   strike_price:"399",
+   offer:"30% OFF",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/haa/h08/27200712081438/S22ASBF727974TE_TEAL_GREY.jpg_1088Wx1632H",
+   name:"Allen Solly",
+   decs:"Crew Neck Cotton Boys T-Shirt",
+   price:"399",
+   strike_price:"499",
+   offer:"20% OFF",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h2c/had/17553443291166/A21INFBSRT66_BLACK.jpg_1088Wx1632H",
+   name:"Infuse",
+   decs:"Solid Cotton Regular Fit Boys Shorts",
+   price:"199",
+   strike_price:"399",
+   offer:"50% OFF",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/hd4/h03/26999517577246/S22GJB739C672BL_BLUE.jpg_1088Wx1632H",
+   name:"Gini & Jony",
+   decs:"Printed Knits Spread Collar Boys Shirt",
+   price:"1049",
+   strike_price:"",
+   offer:"",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h7c/h64/27102494392350/S22710BPTEE004_MANGO.jpg_1088Wx1632H",
+   name:"Stop",
+   decs:"Solid Cotton Collar Neck Boys T-Shirt",
+   price:"399",
+   strike_price:"",
+   offer:"",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h0e/h41/16119826186270/S21710BTEE031_PEACH.jpg_1088Wx1632H",
+   name:"Stop",
+   decs:"Boys Embossing T-Shirt",
+   price:"199",
+   strike_price:"399",
+   offer:"50% OFF",
+
+
+
+},
+
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h94/hc7/27102413946910/S22710BPTEE002_DUSTY_PINK.jpg_1088Wx1632H",
+   name:"Stop",
+   decs:"Solid Cotton Collar Neck Boys T-Shirt",
+   price:"399",
+   strike_price:"",
+   offer:"",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/hc7/hc9/27080732180510/S22ASBF741168LI_LIGHT_BLUE.jpg_1088Wx1632H",
+   name:"Allen Solly",
+   decs:"Solid Cotton Shirt Collar Boys Shirt",
+   price:"799",
+   strike_price:"999",
+   offer:"20% OFF",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h75/h26/16325407801374/206094458_ASSORTED.jpg_1088Wx1632H",
+   name:"Jockey",
+   decs:"Boys Solid Vest - Pack of 2",
+   price:"278",
+   strike_price:"",
+   offer:"",
+
+
+
+}
+
+
+]
+
+localStorage.setItem("boyssWear",JSON.stringify(boysWear)) ;
---
+++ Mohit/Mohitscripts/checkbox.js
@@ -0,0 +1,294 @@
+const checkbox=()=>{
+    return `
+    <div id="checkboxes">
+        <div id="corner">
+        
+           
+            <hr>
+            <!-- first -->
+            <h4>CATEGORIES</h4>                                                     
+            <hr>
+            <div id="firstsort">                                                
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                   
+                    <div>ACCESSORIES  (449)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>CASUAL JACKETS  (90)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>ETHNICWEAR  (1,149)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>INNERWEAR & NIGHTWEAR  (404)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>JEANS  (958)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>SHIRTS  (2,771)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>SHORTS  (177)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>SPORTS & ACTIVEWEAR  (1,087)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>SUITS & BLAZERS  (422)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>T-SHIRTS & POLOS  (2,246)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>TROUSERS  (858)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>WINTERWEAR  (423)</div>
+                </div>   
+            </div>
+            <br>
+            <!-- second -->
+            <h4>BRANDS</h4>
+            <hr>
+            <h4>OFFERS</h4>
+            <hr>
+            <div id="firstsort">
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>FLAT 30% OFF  (25)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>FLAT 40% OFF  (11)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>FLAT 50% OFF  (153)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>FLAT 60% OFF  (12)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>FLAT 15% OFF  (10)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>FLAT 20% OFF  (47))</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>FLAT 25% OFF  (23)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>FLAT 30% OFF  (329)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>FLAT RS.999/-  (15)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>BUY 1 GET 1 FREE  (8)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>FLAT RS.899/-  (2)</div>
+                </div>
+            </div>
+            <br>
+            <!-- third -->
+            <h4>SIZE</h4>
+            <hr>
+            <div id="firstsort">
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>10 (11) </div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>100 (5) </div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>102 (10)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>104 (4)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>108 (4)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>11 (14)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>110 (1)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>12 (15)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>12-14 YEARS  (4)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>13 (13)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>13 (14)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>2.74M X 2.74M  (1)</div>
+                </div> 
+            </div>
+            <br>
+            <!-- fourth -->
+            <h4>COLORS</h4>
+            <hr>
+            <div id="firstsort">
+                <div id="leftsort">
+                    <div class="divColors" id="begie"></div>
+                    <div>BEIGE  (58)</div>
+                </div>
+                <div  id="leftsort">
+                    <div class="divColors" id="black"></div>
+                    <div>BLACK  (1,505) </div>
+                </div>
+                <div id="leftsort">
+                    <div class="divColors" id="blue"></div>
+                    <div>BLUE  (3,181) </div>
+                </div>
+                <div id="leftsort">
+                    <div class="divColors" id="brown"></div>
+                    <div>BROWN  (690)</div>
+                </div>
+                <div id="leftsort">
+                    <div class="divColors" id="green"></div>
+                    <div>GREEN  (1,022) </div>
+                </div>
+                <div id="leftsort">
+                    <div class="divColors" id="grey"></div>
+                    <div>GREY  (924)</div>
+                </div>
+                <div id="leftsort">
+                    <div class="divColors" id="multi"></div>
+                    <div>MULTI  (793)</div>
+                </div>
+                <div id="leftsort">
+                    <div class="divColors" id="orange"></div>
+                    <div>ORANGE  (224)</div>
+                </div>   
+                
+                <div id="leftsort">
+                    <div class="divColors" id="pink"></div>
+                    <div>PINK  (252)</div>
+                </div>
+                <div id="leftsort">
+                    <div class="divColors" id="purple"></div>
+                    <div>PURPLE  (200) </div>
+                </div>
+                <div id="leftsort">
+                    <div class="divColors" id="red"></div>
+                    <div>RED  (790)</div>
+                </div>
+                <div id="leftsort">
+                    <div class="divColors" id="white"></div>
+                    <div>WHITE  (884)</div>
+                </div>
+                <div id="leftsort">
+                    <div class="divColors" id="yellow"></div>
+                    <div>YELLOW  (443)</div>
+                </div>                 
+            </div>
+            <!-- </div> -->
+            
+            <br>
+            <!-- fifth -->
+            <h4>DELIVERY TYPE</h4>
+            <hr>
+            <div class="deliveryDIv" >
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>EXPRESS STORE PICK UP  (10,964)</div>
+                </div>
+                <hr>      
+            </div>
+            <br>
+            <!-- six -->
+            <h4>Price</h4>
+            <hr>
+            <div id="firstsort">
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>51-100  (2)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>101-500  (715)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>501-1000  (1,772)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>1001-2000  (4,404) </div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>2001-3000  (2,810)</div>
+                </div>  
+                
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>3001-5000  (1,038) (2)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>5001-10000  (277)</div>
+                </div>
+                <div id="leftsort">
+                    <input type="checkbox" name="" id="">
+                    <div>10001 AND ABOVE  (57)</div>
+                </div>
+            </div>
+            <br>
+            <!-- seven -->
+            <p id="pricePara">Enter a price range</p>
+            <div id="priceRange">
+                <input type="text" placeholder="₹">
+                
+                <P id="pricePara">to</P>
+                <input type="text" placeholder="₹">
+              
+            </div>
+            <button>REFINE SEARCH</button>
+        </div>
+    
+`
+}
+export {checkbox}
