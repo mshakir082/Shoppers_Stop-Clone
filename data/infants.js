a95b5b8f9a5b65fb467357b4af2b10a626b87689
mshakir082 <mdshakirhussain298@gmail.com>
2022-04-03 19:51:11 +0530

    all data comming

---
+++ data/infants.js
@@ -0,0 +1,308 @@
+
+
+    infantsWear = [
+
+    
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h47/h32/26426550779934/A21700GPDRS916_NAVY.jpg_1088Wx1632H",
+   image1:"https://sslimages.shoppersstop.com/sys-master/images/hfd/hb3/26426263207966/A21700GPDRS916_NAVY_alt1.jpg_1088Wx1632H",
+   image2:"https://sslimages.shoppersstop.com/sys-master/images/he1/h5b/26426254622750/A21700GPDRS916_NAVY_alt2.jpg_1088Wx1632H",
+   name:"Karrot",
+   desc:"Solid Poly Blend Crew Neck Infant Girls Dress",
+   price:"999",
+   strike_price:"",
+   offer:"",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h71/hc7/27102419681310/S22700GDRS222_BLUE.jpg_1088Wx1632H",
+   image1:"https://sslimages.shoppersstop.com/sys-master/images/h7e/hec/27102553112606/S22700GDRS222_BLUE_alt1.jpg_1088Wx1632H",
+   image2:"https://sslimages.shoppersstop.com/sys-master/images/he3/h78/27102598791198/S22700GDRS222_BLUE_alt2.jpg_1088Wx1632H",
+   name:"Karrot",
+   desc:"Printed Cotton Crew Neck Infant Girls Dress",
+   price:"299",
+   strike_price:"",
+   offer:"",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/hf5/h6a/16554018635806/S21700BTEE247_RED.jpg_1088Wx1632H",
+   image1:"https://sslimages.shoppersstop.com/sys-master/images/h4b/ha7/16552917958686/S21700BTEE247_RED_alt1.jpg_1088Wx1632H",
+   image2:"https://sslimages.shoppersstop.com/sys-master/images/h26/ha2/16553530785822/S21700BTEE247_RED_alt2.jpg_1088Wx1632H",
+   name:"Karrot",
+   desc:"Printed Cotton Crew Neck Infant Boys T-Shirt",
+   price:"99",
+   strike_price:"199",
+   offer:"50% OFF",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h70/h81/16552296087582/S21700BTEE260_ROYAL.jpg_1088Wx1632H",
+   image1:"https://sslimages.shoppersstop.com/sys-master/images/h2f/hd5/16552593752094/S21700BTEE260_ROYAL_alt1.jpg_1088Wx1632H",
+   image2:"https://sslimages.shoppersstop.com/sys-master/images/h9c/h52/16552796258334/S21700BTEE260_ROYAL_alt2.jpg_1088Wx1632H",
+   name:"Karrot",
+   desc:"Printed Cotton Crew Neck Infant Boys T-Shirt",
+   price:"99",
+   strike_price:"199",
+   offer:"50% OFF",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h75/h56/16553249570846/S21700BTEE293_PEACH.jpg_1088Wx1632H",
+   image1:"https://sslimages.shoppersstop.com/sys-master/images/hbc/hb6/16553862103070/S21700BTEE293_PEACH_alt1.jpg_1088Wx1632H",
+   image2:"https://sslimages.shoppersstop.com/sys-master/images/h64/h5b/16554050617374/S21700BTEE293_PEACH_alt2.jpg_1088Wx1632H",
+   name:"Karrot",
+   desc:"Printed Cotton Crew Neck Infant Boys T-Shirt",
+   price:"99",
+   strike_price:"199",
+   offer:"50% OFF",
+
+
+
+},
+
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/hc2/he1/16552829812766/S21700GTOP138_WHITE.jpg_1088Wx1632H",
+   image1:"https://sslimages.shoppersstop.com/sys-master/images/hd2/h4f/16553628958750/S21700GTOP138_WHITE_alt1.jpg_1088Wx1632H",
+   image2:"https://sslimages.shoppersstop.com/sys-master/images/h13/h38/16552249786398/S21700GTOP138_WHITE_alt2.jpg_1088Wx1632H",
+   name:"Karrot",
+   desc:"Solid Cotton Crew Neck Infant Girls Tops",
+   price:"249",
+   strike_price:"Rs 499",
+   offer:"50% OFF",
+
+
+
+},
+
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/hc2/he1/16552829812766/S21700GTOP138_WHITE.jpg_1088Wx1632H",
+   image1:"https://sslimages.shoppersstop.com/sys-master/images/hd2/h4f/16553628958750/S21700GTOP138_WHITE_alt1.jpg_1088Wx1632H",
+   image2:"50% OFF",
+   name:"Karrot",
+   desc:"Solid Cotton Crew Neck Infant Girls Tops",
+   price:"249",
+   strike_price:"499",
+   offer:"50% OFF",
+
+
+
+},
+
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h3e/h9d/16552028438558/S21700BTEE405_ECRU.jpg_1088Wx1632H",
+   image1:"https://sslimages.shoppersstop.com/sys-master/images/h7f/h5f/16552649457694/S21700BTEE405_ECRU_alt1.jpg_1088Wx1632H",
+   image2:"https://sslimages.shoppersstop.com/sys-master/images/he5/h93/16553097592862/S21700BTEE405_ECRU_alt2.jpg_1088Wx1632H",
+   name:"Karrot",
+   desc:"Printed Cotton Crew Neck Infant Boys T-Shirt",
+   price:"99",
+   strike_price:"199",
+   offer:"50% OFF",
+
+
+
+},
+
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/ha1/hd3/16455736688670/S21700BTEE307_YELLOW.jpg_1088Wx1632H",
+   image1:"https://sslimages.shoppersstop.com/sys-master/images/he1/h59/16455562723358/S21700BTEE307_YELLOW_alt1.jpg_1088Wx1632H",
+   image2:"https://sslimages.shoppersstop.com/sys-master/images/h24/h05/16455869497374/S21700BTEE307_YELLOW_alt2.jpg_1088Wx1632H",
+   name:"Karrot",
+   desc:"Printed Cotton Knitted Fabric Crew Neck Boys T-Shirt",
+   price:"99",
+   strike_price:"199",
+   offer:"50% OFF",
+
+
+
+},
+
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/hee/he9/17318301663262/S21700GSHRT310_BLUE.jpg_1088Wx1632H",
+   image1:"https://sslimages.shoppersstop.com/sys-master/images/h8b/h78/17318509183006/S21700GSHRT310_BLUE_alt1.jpg_1088Wx1632H",
+   image2:"https://sslimages.shoppersstop.com/sys-master/images/h68/h26/17318317228062/S21700GSHRT310_BLUE_alt2.jpg_1088Wx1632H",
+   name:"Karrot",
+   desc:"Printed Cotton Infant Girls Shorts",
+   price:"139",
+   strike_price:"199",
+   offer:"30% OFF",
+
+
+
+},
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h19/hc2/16585472016414/FS21700BBRF222_WHITE.jpg_1088Wx1632H",
+   image1:"https://sslimages.shoppersstop.com/sys-master/images/h38/hd0/16585562652702/FS21700BBRF222_WHITE_alt1.jpg_1088Wx1632H",
+   image2:"https://sslimages.shoppersstop.com/sys-master/images/h04/h9b/16585610166302/FS21700BBRF222_WHITE_alt2.jpg_1088Wx1632H",
+   name:"Karrot",
+   desc:"Printed Cotton Infant Boys Briefs",
+   price:"149",
+   strike_price:"299",
+   offer:"50% OFF",
+
+
+
+},
+
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h14/hcf/16552345042974/S21700GTEE530_NAVY.jpg_1088Wx1632H",
+   image1:"https://sslimages.shoppersstop.com/sys-master/images/hfa/ha1/16553875374110/S21700GTEE530_NAVY_alt1.jpg_1088Wx1632H",
+   image2:"https://sslimages.shoppersstop.com/sys-master/images/h29/h70/16551621656606/S21700GTEE530_NAVY_alt2.jpg_1088Wx1632H",
+   name:"Karrot",
+   desc:"Solid Cotton Crew Neck Infant Girls T-Shirt",
+   price:"149",
+   strike_price:"299",
+   offer:"50% OFF",
+
+
+
+},
+
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h9c/haf/26906394263582/S22700GDRS215_WHITE.jpg_1088Wx1632H",
+   image1:"https://sslimages.shoppersstop.com/sys-master/images/ha4/h75/26906396491806/S22700GDRS215_WHITE_alt1.jpg_1088Wx1632H",
+   image2:"https://sslimages.shoppersstop.com/sys-master/images/h79/h34/26906411958302/S22700GDRS215_WHITE_alt2.jpg_1088Wx1632H",
+   name:"Karrot",
+   desc:"Embroidered Cotton Crew Neck Infant Girls Dress",
+   price:"699",
+   strike_price:"",
+   offer:"",
+
+
+
+},
+
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h8b/h3c/16553189670942/S21700GTEE513_CORAL.jpg_1088Wx1632H",
+   image1:"https://sslimages.shoppersstop.com/sys-master/images/hc6/h0c/16554114842654/S21700GTEE513_CORAL_alt1.jpg_1088Wx1632H",
+   image2:"https://sslimages.shoppersstop.com/sys-master/images/h75/he5/16552001634334/S21700GTEE513_CORAL_alt2.jpg_1088Wx1632H",
+   name:"Karrot",
+   desc:"Printed Cotton Crew Neck Infant Girls T-Shirt",
+   price:"99",
+   strike_price:"199",
+   offer:"50% OFF",
+
+
+
+},
+
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h29/hc3/16552629534750/S21700GTEE505_WHITE.jpg_1088Wx1632H",
+   image1:"https://sslimages.shoppersstop.com/sys-master/images/h1a/h00/16553224798238/S21700GTEE505_WHITE_alt1.jpg_1088Wx1632H",
+   image2:"https://sslimages.shoppersstop.com/sys-master/images/h79/h0d/16552546238494/S21700GTEE505_WHITE_alt2.jpg_1088Wx1632H",
+   name:"Karrot",
+   desc:"Printed Cotton Crew Neck Infant Girls T-Shirt",
+   price:"99",
+   strike_price:"199",
+   offer:"50% OFF",
+
+
+
+},
+
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h3f/h05/26906397999134/S22700GDRS221_MULTI.jpg_1088Wx1632H",
+   image1:"https://sslimages.shoppersstop.com/sys-master/images/hc9/h5e/26906275643422/S22700GDRS221_MULTI_alt1.jpg_1088Wx1632H",
+   image2:"https://sslimages.shoppersstop.com/sys-master/images/h6f/h67/26906278527006/S22700GDRS221_MULTI_alt2.jpg_1088Wx1632H",
+   name:"Karrot",
+   desc:"Printed Cotton Crew Neck Infant Girls Dress",
+   price:"299",
+   strike_price:"",
+   offer:"",
+
+
+
+},
+
+
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h47/he0/16585635463198/S21700BTEE250_ECRU.jpg_1088Wx1632H",
+   image1:"https://sslimages.shoppersstop.com/sys-master/images/h43/h6d/16585555312670/S21700BTEE250_ECRU_alt1.jpg_1088Wx1632H",
+   image2:"https://sslimages.shoppersstop.com/sys-master/images/ha3/hb8/16585305915422/S21700BTEE250_ECRU_alt2.jpg_1088Wx1632H",
+   name:"Karrot",
+   desc:"Striped Cotton Crew Neck Infant Boys T-Shirt",
+   price:"99",
+   strike_price:"199",
+   offer:"50% OFF",
+
+
+
+},
+
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h45/h8b/16552283701278/FS21700GSLIP093_ASSORTED.jpg_1088Wx1632H",
+   image1:"https://sslimages.shoppersstop.com/sys-master/images/h59/h7e/16552655290398/FS21700GSLIP093_ASSORTED_alt1.jpg_1088Wx1632H",
+   image2:"https://sslimages.shoppersstop.com/sys-master/images/hd5/h4d/16551529545758/FS21700GSLIP093_ASSORTED_alt2.jpg_1088Wx1632H",
+   name:"Karrot",
+   desc:"Printed Cotton Crew Neck Infant Girls Slip",
+   price:"149",
+   strike_price:"299",
+   offer:"50% OFF",
+
+
+
+},
+
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h41/ha3/26504255275038/A21700GPDRS915_FUSCHIA.jpg_1088Wx1632H",
+   image1:"https://sslimages.s3-ap-southeast-1.amazonaws.com/sys-master/images/hb4/ha0/26504333459486/A21700GPDRS915_FUSCHIA_alt1.jpg_1088Wx1632H",
+   image2:"https://sslimages.shoppersstop.com/sys-master/images/h25/h99/26504336343070/A21700GPDRS915_FUSCHIA_alt2.jpg_1088Wx1632H",
+   name:"Karrot",
+   desc:"Solid Poly Blend Crew Neck Infant Girls Dress",
+   price:"999",
+   strike_price:"",
+   offer:"",
+
+
+
+},
+
+
+
+
+
+{
+   image_url:"https://sslimages.shoppersstop.com/sys-master/images/h53/h5f/16553482879006/FS21700BPYJ185_ASSORTED.jpg_1088Wx1632H",
+   image1:"https://sslimages.s3-ap-southeast-1.amazonaws.com/sys-master/images/hb1/h17/16552635432990/FS21700BPYJ185_ASSORTED_alt1.jpg_1088Wx1632H",
+   image2:"https://sslimages.shoppersstop.com/sys-master/images/hf0/h1d/16553932914718/FS21700BPYJ185_ASSORTED_alt2.jpg_1088Wx1632H",
+   name:"Karrot",
+   desc:"Printed Cotton Regular Fit Infant Boys Pyjama",
+   price:"249",
+   strike_price:"499",
+   offer:"50% OFF",
+
+
+
+},
+  
+           
+    ];
+
+
+    localStorage.setItem("infantsWear",JSON.stringify(infantsWear)) ;
